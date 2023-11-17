5670b2f10aa665ebc94216754648fa33bfa52b5a
Estebancott <estebancott@gmail.com>
2023-11-17 17:00:01 +0100

    Update ArduinoMotors.ino with faster digital write library and STM32 timer interruption

--- ArduinoMotors.ino
+++ ArduinoMotors.ino
@@ -12,8 +12,8 @@
 
 #include <Wire.h>
 #include <PID_v1.h>                                  // PID
-#include "digitalWriteFast-1.2.0/digitalWriteFast.h" // Read and Write Faster than Arduino
-// #include "TimerInterrupt-1.8.0\src\TimerInterrupt.h" //time interuption
+#include <digitalWriteFast.h> // Read and Write Faster than Arduino
+#include <STM32TimerInterrupt.h> //time interuption
 
 // Comment or uncomment to activate
 #define DEBUG // Used to print informations to the serial port
